87549927cbb2959f2047be42b7cd6ebe79c33aac
Eddie Welker <eddie.welker@gmail.com>
2025-12-14 17:04:29 -0500

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,20 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eddiewelker.com",
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [
+      sitemap(),
+      mdx(),
+      pagefind(),
+      partytown({
+        // This is necessary to tell Partytown to handle GA events
+        forward: ['dataLayer.push'],
+      }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
