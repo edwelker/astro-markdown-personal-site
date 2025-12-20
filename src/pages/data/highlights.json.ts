055b6d1bea0cebb4c65f6ccad0343ffc7a6b3885
Eddie Welker <eddie.welker@gmail.com>
2025-12-20 18:28:50 -0500

    update api

---
+++ src/pages/data/highlights.json.ts
@@ -0,0 +1,10 @@
+import { highlights } from "../../data/highlights";
+
+export async function GET() {
+  return new Response(JSON.stringify(highlights), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
