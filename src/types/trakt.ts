e3ad47cbbeded37368718d5df398108e51a2edd0
Eddie Welker <eddie.welker@gmail.com>
2025-12-20 22:44:12 -0500

    media

---
+++ src/types/trakt.ts
@@ -0,0 +1,28 @@
+export interface TraktRating {
+  rated_at: string;
+  rating: number;
+  type: "movie" | "show" | "episode";
+  poster?: string;
+  movie?: {
+    title: string;
+    year: number;
+    ids: { trakt: number; slug: string; imdb: string; tmdb: number };
+  };
+  show?: {
+    title: string;
+    year: number;
+    ids: { trakt: number; slug: string; imdb: string; tmdb: number };
+  };
+}
+
+export interface HeatmapCell {
+  decade: number;
+  rating: number;
+  count: number;
+}
+
+export interface TraktData {
+  lastUpdated: string;
+  recentRatings: TraktRating[];
+  heatmap: HeatmapCell[];
+}
