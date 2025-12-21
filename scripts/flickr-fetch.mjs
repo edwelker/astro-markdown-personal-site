432ab8d3c09ab7b870b5f43e3df0693c7309fc71
Eddie Welker <eddie.welker@gmail.com>
2025-12-21 12:35:46 -0500

    modify flickr on homepage

--- scripts/flickr-fetch.mjs
+++ scripts/flickr-fetch.mjs
@@ -1,8 +1,10 @@
 import { runETL } from './lib-etl.mjs';
 import { transformFlickrData } from './flickr-logic.mjs';
+
 runETL({
   name: 'Flickr',
-  url: `https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${process.env.FLICKR_API_KEY}&user_id=72923429@N00&format=json&nojsoncallback=1&extras=url_m,date_taken,tags`,
+  // Added o_dims and width_m, height_m to extras
+  url: `https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${process.env.FLICKR_API_KEY}&user_id=72923429@N00&format=json&nojsoncallback=1&extras=url_m,width_m,height_m,date_taken,tags`,
   transform: transformFlickrData,
   outFile: 'src/data/flickr-photos.json',
   defaultData: []
--- scripts/flickr-logic.mjs
+++ scripts/flickr-logic.mjs
@@ -1,6 +1,19 @@
 export const transformFlickrData = (data) => {
   const photos = data?.photos?.photo ?? [];
-  return photos.map(p => ({
-    id: p.id, title: p.title || 'Untitled', src: p.url_m ?? p.url_l ?? '', date: p.datetaken ?? '', tags: p.tags ? p.tags.split(' ') : []
-  }));
+  return photos.map(p => {
+    // API provides url_m (medium) and its dimensions
+    const width = parseInt(p.width_m || "1");
+    const height = parseInt(p.height_m || "1");
+    
+    return {
+      id: p.id, 
+      title: p.title || 'Untitled', 
+      src: p.url_m ?? p.url_l ?? '', 
+      date: p.datetaken ?? '', 
+      tags: p.tags ? p.tags.split(' ') : [],
+      aspect: width / height,
+      isLandscape: width > height,
+      isPortrait: height > width
+    };
+  });
 };
