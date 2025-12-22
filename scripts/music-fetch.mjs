273e72bac83eab6a3c98a8eb1f76127502e9c214
Eddie Welker <eddie.welker@gmail.com>
2025-12-21 23:22:59 -0500

    update music

--- scripts/music-fetch.mjs
+++ scripts/music-fetch.mjs
@@ -6,21 +6,21 @@ async function run() {
   try {
     const res = await Promise.all([
       fetch(`${base}?method=user.getinfo&${params}`),
-      // 7 Day
+      // 7 Day (Requesting 10 to ensure layout flexibility)
       fetch(`${base}?method=user.gettopartists&period=7day&limit=10&${params}`),
-      fetch(`${base}?method=user.gettopalbums&period=7day&limit=6&${params}`),
+      fetch(`${base}?method=user.gettopalbums&period=7day&limit=10&${params}`),
       fetch(`${base}?method=user.gettoptracks&period=7day&limit=10&${params}`),
       // 1 Month
       fetch(`${base}?method=user.gettopartists&period=1month&limit=10&${params}`),
-      fetch(`${base}?method=user.gettopalbums&period=1month&limit=6&${params}`),
+      fetch(`${base}?method=user.gettopalbums&period=1month&limit=10&${params}`),
       fetch(`${base}?method=user.gettoptracks&period=1month&limit=10&${params}`),
       // 12 Month
       fetch(`${base}?method=user.gettopartists&period=12month&limit=10&${params}`),
-      fetch(`${base}?method=user.gettopalbums&period=12month&limit=6&${params}`),
+      fetch(`${base}?method=user.gettopalbums&period=12month&limit=10&${params}`),
       fetch(`${base}?method=user.gettoptracks&period=12month&limit=10&${params}`),
       // Overall
       fetch(`${base}?method=user.gettopartists&period=overall&limit=10&${params}`),
-      fetch(`${base}?method=user.gettopalbums&period=overall&limit=6&${params}`),
+      fetch(`${base}?method=user.gettopalbums&period=overall&limit=10&${params}`),
       fetch(`${base}?method=user.gettoptracks&period=overall&limit=10&${params}`)
     ]);
     const json = await Promise.all(res.map(r => r.json()));
