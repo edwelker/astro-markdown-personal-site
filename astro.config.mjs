cb89037f692d2c3dc56e51fd93b50fb1b98efecd
Eddie Welker <eddie.welker@gmail.com>
2026-01-01 11:15:01 -0500

    see if this will fix SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,6 +43,11 @@ export default defineConfig({
   ],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "node:path/posix": "path-browserify",
+      },
+    },
     build: {
       // Prevent Vite from inlining assets (including CSS) as base64
       assetsInlineLimit: 0,
